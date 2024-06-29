5113db170910cc930ce37c9e3ab57f57386677fb
leiweibau <105860611+leiweibau@users.noreply.github.com>
2024-06-29 21:36:07 +0200

    Update Notes: 2024-06-29
    - Pi.Alert v2024-06-29 with Satellite support released

--- tar/create.sh
+++ tar/create.sh
@@ -13,5 +13,3 @@ rm pialert_satellite/tar/pialert_satellite*.tar
 # ------------------------------------------------------------------------------
 tar cvf pialert_satellite/tar/pialert_satellite_latest.tar --no-mac-metadata --no-xattrs --exclude="pialert_satellite/tar" --exclude="pialert_satellite/.git" --exclude="pialert_satellite/.github" --exclude=".gitignore" --exclude=".DS_Store" pialert_satellite | wc -l
 
-#ln -s pialert_$PIALERT_VERSION.tar pialert/package/pialert_latest.tar
-#ls -l pialert/package/pialert*.tar
